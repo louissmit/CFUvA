4a711b9e5ed75556c6ee3bc44d9446685d122552
henning <helange23@gmail.com>
2014-02-19 15:55:41 +0100

    jo

--- ex2.py
+++ ex2.py
@@ -6,3 +6,18 @@ This temporary script file is located here:
 /home/henning/.spyder2/.temp.py
 """
 
+import numpy
+import random
+
+
+def computeMCValuation(k=99, s0=100, r=0.06, v=0.2, T=1, M=100000):
+    sols = numpy.zeros((M,1))
+    
+    for i in xrange(0,M):
+        Z = random.gauss(0,1)
+        sn = s0*numpy.exp((r-0.5*pow(v,2))*T + v*sqrt(T)*Z)
+        
+        sols[i] = max(0,sn-k)*numpy.exp(-r*T)
+        
+    
+    return numpy.mean(sols), numpy.var(sols)/sqrt(M)
